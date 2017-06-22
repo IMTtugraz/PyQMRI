9e9b9119c4858e0350771c2b31ecbff7d3575bd2
Oliver Maier <oliver.maier@tugraz.at>
2017-06-22 15:29:14 +0200

    fixed some errors in 3D gradient

--- IRLL_Model.pyx
+++ IRLL_Model.pyx
@@ -211,8 +211,8 @@ cdef class IRLL_Model:
               n_i = i*self.Nproj+j+1
               S[i,j,k,m,n] = (x[0,k,m,n]*self.M0_sc*sin_phi[k,m,n]*(((cexp(-self.TR/(x[1,k,m,n]*self.T1_sc)) - 
                       2*cexp(-self.td/(x[1,k,m,n]*self.T1_sc)) + (cexp(-self.TR/(x[1,k,m,n]*self.T1_sc))
-                      *cexp(-self.td/(x[1,k,m,n]*self.T1_sc))*cos_phi[islice,k,m,n]*((cexp(-self.tau/(x[1,k,m,n]
-                      *self.T1_sc))*cos_phi[islice,k,m,n])**(self.NLL - 1) - 1)*(cexp(-self.tau/(x[1,k,m,n]*
+                      *cexp(-self.td/(x[1,k,m,n]*self.T1_sc))*cos_phi[k,k,m,n]*((cexp(-self.tau/(x[1,k,m,n]
+                      *self.T1_sc))*cos_phi[k,k,m,n])**(self.NLL - 1) - 1)*(cexp(-self.tau/(x[1,k,m,n]*
                       self.T1_sc)) - 1))/(cexp(-self.tau/(x[1,k,m,n]*self.T1_sc))*cos_phi[k,m,n] - 1)
                       + 1)/(cexp(-self.TR/(x[1,k,m,n]*self.T1_sc))*cexp(-self.td/(x[1,k,m,n]*self.T1_sc))
                       *cos_phi[k,m,n]*(cexp(-self.tau/(x[1,k,m,n]*self.T1_sc))
@@ -287,7 +287,7 @@ cdef class IRLL_Model:
                                   *(TAU1 - 1))/(x[1,k,m,n]
                                   **2*T1_sc*(TAU1cosphi - 1)) + (td
                                   *TRTD1
-                                  *cos_phi[islice,m,n]*((TAU1cosphi)**(NLL - 1) - 1)
+                                  *cos_phi[k,m,n]*((TAU1cosphi)**(NLL - 1) - 1)
                                   *(TAU1 - 1))
                                   /(x[1,k,m,n]**2*T1_sc*(TAU1cosphi - 1)) + (tau
                                   *cexp(-(TR + tau + td)/(x[1,k,m,n]*T1_sc))
