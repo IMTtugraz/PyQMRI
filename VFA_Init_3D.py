9dbfba5036e3d45b2da1e6d66ff65f255ec5c152
Oliver <oliver.maier@tugraz.at>
2018-01-19 09:36:02 +0100

    test

--- VFA_Init_3D.py
+++ VFA_Init_3D.py
@@ -75,7 +75,7 @@ dcf = file['dcf'][()].astype(DTYPE)
 
 dimX, dimY, NSlice = (file.attrs['image_dimensions']).astype(int)
 
-reco_Slices = 5
+reco_Slices = 52
 #Create par struct to store everyting
 class struct:
     pass
@@ -313,7 +313,7 @@ irgn_par.display_iterations = True
 
 opt_t.irgn_par = irgn_par
 
-opt_t.execute_3D()
+#opt_t.execute_3D()
 
 ###############################################################################
 ## New .hdf5 save files #######################################################
@@ -328,24 +328,24 @@ os.chdir("output/"+ outdir)
 f = h5py.File("output_"+name,"w")
 dset_result=f.create_dataset("full_result",opt.result.shape,\
                              dtype=np.complex64,data=opt.result)
-dset_result_ref=f.create_dataset("ref_full_result",opt_t.result.shape,\
-                                 dtype=np.complex64,data=opt_t.result)
+#dset_result_ref=f.create_dataset("ref_full_result",opt_t.result.shape,\
+#                                 dtype=np.complex64,data=opt_t.result)
 dset_T1=f.create_dataset("T1_final",np.squeeze(opt.result[-1,1,...]).shape,\
                          dtype=np.complex64,\
                          data=np.squeeze(opt.result[-1,1,...]))
 dset_M0=f.create_dataset("M0_final",np.squeeze(opt.result[-1,0,...]).shape,\
                          dtype=np.complex64,\
                          data=np.squeeze(opt.result[-1,0,...]))
-dset_T1_ref=f.create_dataset("T1_ref",np.squeeze(opt_t.result[-1,1,...]).shape\
-                             ,dtype=np.complex64,\
-                             data=np.squeeze(opt_t.result[-1,1,...]))
-dset_M0_ref=f.create_dataset("M0_ref",np.squeeze(opt_t.result[-1,0,...]).shape\
-                             ,dtype=np.complex64,\
-                             data=np.squeeze(opt_t.result[-1,0,...]))
-f.create_dataset("T1_guess",np.squeeze(model.T1_guess).shape,\
-                 dtype=np.float64,data=np.squeeze(model.T1_guess))
-f.create_dataset("M0_guess",np.squeeze(model.M0_guess).shape,\
-                 dtype=np.float64,data=np.squeeze(model.M0_guess))
+#dset_T1_ref=f.create_dataset("T1_ref",np.squeeze(opt_t.result[-1,1,...]).shape\
+#                             ,dtype=np.complex64,\
+#                             data=np.squeeze(opt_t.result[-1,1,...]))
+#dset_M0_ref=f.create_dataset("M0_ref",np.squeeze(opt_t.result[-1,0,...]).shape\
+#                             ,dtype=np.complex64,\
+#                             data=np.squeeze(opt_t.result[-1,0,...]))
+#f.create_dataset("T1_guess",np.squeeze(model.T1_guess).shape,\
+#                 dtype=np.float64,data=np.squeeze(model.T1_guess))
+#f.create_dataset("M0_guess",np.squeeze(model.M0_guess).shape,\
+#                 dtype=np.float64,data=np.squeeze(model.M0_guess))
 dset_result.attrs['data_norm'] = dscale
 dset_result.attrs['dcf_scaling'] = (N*(np.pi/(4*Nproj)))
 f.flush()
