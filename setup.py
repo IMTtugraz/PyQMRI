a1f1594bd25dbe7ef07e0a57d3ccb69f46175e72
Maier, Oliver <oliver.maier@tugraz.at>
2019-07-30 09:28:20 +0200

    polishing for pypi release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
     
 setup(name='pyqmri',
-      version='0.1.2',
+      version='0.2.0',
       description='Model-based parameter quantification using OpenCL and Python',
       long_description=long_description,
       long_description_content_type="text/markdown",
