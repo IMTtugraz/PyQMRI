fixed gradient scaling

pyqmri/irgn/reco.py
@@ -128,7 +128,7 @@ class ModelReco:
             start = time.time()
             self.grad_x = np.nan_to_num(self.model.execute_gradient(result))
             self._balanceModelGradients(result, ign)
-            self.grad_op.updateRatio(result)
+            self.pdop.grad_op.updateRatio(result)
 
             self.step_val = np.nan_to_num(self.model.execute_forward(result))
             self.grad_buf = cl.Buffer(self._ctx,

pyqmri/pyqmri.py
@@ -102,7 +102,6 @@ def _setupOCL(myargs, par):
 
 def _genImages(myargs, par, data):
     FFT = utils.NUFFT(par, trafo=myargs.trafo, SMS=myargs.sms)
-    #    data = data/(FFT.deblurring[None, None, None, :, None])
     import pyopencl.array as clarray
 
     def nFTH(x, fft, par):
@@ -132,8 +131,8 @@ def _genImages(myargs, par, data):
                         requirements='C')
     del FFT, nFTH
 
-#    del par["file"]["images_cg"]
-#    if "images_cg" not in list(par["file"].keys()):
+#    del par["file"]["images"]
+#    if "images" not in list(par["file"].keys()):
 #        images = np.zeros((par["NScan"],
 #                           par["NSlice"],
 #                           par["dimY"],
@@ -167,18 +166,18 @@ def _genImages(myargs, par, data):
 #                        data[-np.mod(par["NScan"], 10):, ...],
 #                        tol=1e-3)
 #            del cgs
-#        par["file"].create_dataset("images_cg", images.shape,
+#        par["file"].create_dataset("images", images.shape,
 #                                   dtype=DTYPE, data=images)
 #    else:
-#        images = par["file"]['images_cg']
+#        images = par["file"]['images']
 #        if images.shape[1] < par["NSlice"]:
-#            del par["file"]["images_cg"]
+#            del par["file"]["images"]
 #            images = _genImages(myargs, par, data)
 #        else:
 #            print("Using precomputed images")
-#            slices_images = par["file"]['images_cg'][()].shape[1]
+#            slices_images = par["file"]['images'][()].shape[1]
 #            images = \
-#                par["file"]['images_cg'][
+#                par["file"]['images'][
 #                    :, int(slices_images / 2) - int(
 #                        np.floor((par["NSlice"]) / 2)):int(
 #                            slices_images / 2) + int(
@@ -288,7 +287,7 @@ def _start_recon(myargs):
             spec = importlib.util.spec_from_file_location(
                     sig_model_path.split(os.sep)[-1]+'.py',
                     sig_model_path+'.py')
-        elif os.path.splitext(sig_model_path) == '.py':
+        elif os.path.splitext(sig_model_path)[-1] == '.py':
             spec = importlib.util.spec_from_file_location(
                     sig_model_path.split(os.sep)[-1], sig_model_path)
         else:

pyqmri/solver.py
@@ -260,13 +260,6 @@ class PDSolver:
             self.run = self.runTV3D
         else:
             self.run = self.runTGV3D
-        self._ratio = clarray.to_device(
-            self._queue,
-            (1 /
-             self.unknowns *
-             np.ones(
-                 self.unknowns)).astype(
-                dtype=DTYPE_real))
 
     def __del__(self):
         """ Destructor
@@ -329,7 +322,7 @@ class PDSolver:
                                         [r, z1,
                                          self._coil_buf,
                                          self.grad_buf,
-                                         self._ratio]))
+                                         self.grad_op._ratio]))
         Kyk2.add_event(self.update_Kyk2(Kyk2, z2, z1))
 
         for i in range(iters):
@@ -364,7 +357,7 @@ class PDSolver:
                                                     [r_new, z1_new,
                                                      self._coil_buf,
                                                      self.grad_buf,
-                                                     self._ratio]))
+                                                     self.grad_op._ratio]))
                 Kyk2_new.add_event(self.update_Kyk2(Kyk2_new, z2_new, z1_new))
 
                 ynorm = (
@@ -683,7 +676,7 @@ class PDSolver:
                                         [r, z1,
                                          self._coil_buf,
                                          self.grad_buf,
-                                         self._ratio]))
+                                         self.grad_op._ratio]))
 
         for i in range(iters):
             x_new.add_event(self.update_primal(x_new, x, Kyk1, xk, tau,
@@ -713,7 +706,7 @@ class PDSolver:
                                                     [r_new, z1_new,
                                                      self._coil_buf,
                                                      self.grad_buf,
-                                                     self._ratio]))
+                                                     self.grad_op._ratio]))
 
                 ynorm = ((clarray.vdot(r_new - r, r_new - r) +
                           clarray.vdot(z1_new - z1, z1_new - z1))**(1 / 2)