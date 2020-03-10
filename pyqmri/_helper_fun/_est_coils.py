Fixed scaling and added FFC

pyqmri/_helper_fun/_est_coils.py
@@ -231,7 +231,6 @@ def est_coils(data, par, file, args, off):
                 par["C"] = par["C"] / \
                     np.tile(sumSqrC, (par["NC"], 1, 1, 1))
             del file['Coils']
-
             file.create_dataset(
                 "Coils",
                 par["C"].shape,
@@ -414,7 +413,6 @@ def est_coils(data, par, file, args, off):
                 if not nlinvRealConstr:
                     par["phase"][i, :, :] = np.exp(
                         1j * np.angle(result[i].get()[0, -1, :, :]))
-
                     # standardize coil sensitivity profiles
             sumSqrC = np.sqrt(
                 np.sum(
@@ -424,7 +422,7 @@ def est_coils(data, par, file, args, off):
                     0))  # 4, 9, 128, 128
             par["InScale"] = sumSqrC
             if par["NC"] == 1:
-                par["C"] = sumSqrC
+                par["C"] = sumSqrC[None,...]
             else:
                 par["C"] = par["C"] / np.tile(sumSqrC, (par["NC"], 1, 1, 1))
         file.create_dataset(

pyqmri/irgn.py
@@ -58,23 +58,23 @@ class IRGNOptimizer:
         self.reg_type = reg_type
         self._prg = []
         self.num_dev = len(par["num_dev"])
-        self.par_slices = par["par_slices"]
+        self.NSlice = par["NSlice"]
         self.streamed = streamed
         self._imagespace = imagespace
         self._SMS = SMS
-        if streamed and par["NSlice"]/(self.num_dev*self.par_slices) < 2:
+        if streamed and par["NSlice"]/(self.num_dev*par["par_slices"]) < 2:
             raise ValueError(
                 "Number of Slices devided by parallel "
                 "computed slices and devices needs to be larger two.\n"
                 "Current values are %i total Slices, %i parallel slices and "
                 "%i compute devices."
-                % (par["NSlice"], self.par_slices, self.num_dev))
-        if streamed and par["NSlice"] % self.par_slices:
+                % (par["NSlice"], par["par_slices"], self.num_dev))
+        if streamed and par["NSlice"] % par["par_slices"]:
             raise ValueError(
                 "Number of Slices devided by parallel "
                 "computed slices needs to be an integer.\n"
                 "Current values are %i total Slices with %i parallel slices."
-                % (par["NSlice"], self.par_slices))
+                % (par["NSlice"], par["par_slices"]))
         if DTYPE == np.complex128:
             if streamed:
                 kernname = 'kernels/OpenCL_Kernels_double_streamed.c'
@@ -212,6 +212,7 @@ class IRGNOptimizer:
                 requirements='C')
 
         self.step_val = np.nan_to_num(self.model.execute_forward(result))
+
         if self.streamed:
             if self._SMS is False:
                 self.step_val = np.require(
@@ -222,9 +223,8 @@ class IRGNOptimizer:
             self.modelgrad = np.nan_to_num(
                 self.model.execute_gradient(result))
 
-            self._balanceModelGradients(result, ign)
-            if ign > 0:
-                self.pdop.grad_op.updateRatio(result)
+            self._balanceModelGradients(result)
+            # self.pdop.grad_op.updateRatio(result)
 
             self.step_val = np.nan_to_num(self.model.execute_forward(result))
 
@@ -235,11 +235,13 @@ class IRGNOptimizer:
                 self.modelgrad = np.require(
                     np.transpose(self.modelgrad, self.data_trans_axes),
                     requirements='C')
+                self.pdop.model = self.model
                 self.pdop.modelgrad = self.modelgrad
             else:
                 self.modelgrad = clarray.to_device(
                     self._queue[0],
                     self.modelgrad)
+                self.pdop.model = self.model
                 self.pdop.modelgrad = self.modelgrad
 
             self._updateIRGNRegPar(result, ign)
@@ -259,14 +261,10 @@ class IRGNOptimizer:
         self._calcResidual(result, self.data, ign+1)
 
     def _updateIRGNRegPar(self, result, ign):
-        self.irgn_par["delta_max"] = (self.delta_max /
-                                      1e3 *
-                                      np.linalg.norm(result))
         self.irgn_par["delta"] = np.minimum(
-            self.delta /
-            (1e3) *
-            np.linalg.norm(result)*self.irgn_par["delta_inc"]**ign,
-            self.irgn_par["delta_max"])
+            self.delta
+            * self.irgn_par["delta_inc"]**ign,
+            self.irgn_par["delta_max"]/np.sqrt(self.par["NSlice"]))
         self.irgn_par["gamma"] = np.maximum(
             self.gamma * self.irgn_par["gamma_dec"]**ign,
             self.irgn_par["gamma_min"])
@@ -274,22 +272,28 @@ class IRGNOptimizer:
             self.omega * self.irgn_par["omega_dec"]**ign,
             self.irgn_par["omega_min"])
 
-    def _balanceModelGradients(self, result, ind):
+    def _balanceModelGradients(self, result):
         scale = np.reshape(
             self.modelgrad,
             (self.par["unknowns"],
              self.par["NScan"] * self.par["NSlice"] *
              self.par["dimY"] * self.par["dimX"]))
         scale = np.linalg.norm(scale, axis=-1)
-        scale = 1e3 / scale
-        if not np.mod(ind, 1):
-            for uk in range(self.par["unknowns"]):
-                self.model.constraints[uk].update(scale[uk])
-                result[uk, ...] *= self.model.uk_scale[uk]
-                self.modelgrad[uk] /= self.model.uk_scale[uk]
-                self.model.uk_scale[uk] *= scale[uk]
-                result[uk, ...] /= self.model.uk_scale[uk]
-                self.modelgrad[uk] *= self.model.uk_scale[uk]
+        scale /= np.linalg.norm(scale)/np.sqrt(self.par["unknowns"])
+        scale = 1 / scale
+        for uk in range(self.par["unknowns"]):
+            self.model.constraints[uk].update(scale[uk])
+            result[uk, ...] *= self.model.uk_scale[uk]
+            self.modelgrad[uk] /= self.model.uk_scale[uk]
+            self.model.uk_scale[uk] *= scale[uk]
+            result[uk, ...] /= self.model.uk_scale[uk]
+            self.modelgrad[uk] *= self.model.uk_scale[uk]
+        scale = np.reshape(
+            self.modelgrad,
+            (self.par["unknowns"],
+             self.par["NScan"] * self.par["NSlice"] *
+             self.par["dimY"] * self.par["dimX"]))
+
 ###############################################################################
 # New .hdf5 save files ########################################################
 ###############################################################################
@@ -369,6 +373,7 @@ class IRGNOptimizer:
 
         self._fval = (datacost +
                       regcost +
+                      L2Cost +
                       self.irgn_par["omega"] / 2 *
                       np.linalg.norm(grad_H1)**2)
         del grad_tv, grad_H1
@@ -378,9 +383,9 @@ class IRGNOptimizer:
             self.pdop._fval_init = self._fval
 
         print("-" * 75)
-        print("Costs of Data: %f" % (datacost))
-        print("Costs of T(G)V: %f" % (regcost))
-        print("Costs of L2 Term: %f" % (L2Cost))
+        print("Costs of Data: %f" % (datacost * 1e3))
+        print("Costs of T(G)V: %f" % (regcost * 1e3))
+        print("Costs of L2 Term: %f" % (L2Cost * 1e3))
         print("-" * 75)
         print("Function value at GN-Step %i: %f" %
               (GN_it, 1e3*self._fval / self._fval_init))
@@ -461,9 +466,9 @@ class IRGNOptimizer:
                   3D can be used with a single slice.")
             raise NotImplementedError
         else:
-            self.irgn_par["lambd"] *= 1e2/np.sqrt(self.par["SNR_est"])
-            self.delta = self.irgn_par["delta"]
-            self.delta_max = self.irgn_par["delta_max"]
+            self.irgn_par["lambd"] *= (np.sqrt(self.par["NSlice"])
+                                       / np.sqrt(self.par["SNR_est"]))
             self.gamma = self.irgn_par["gamma"]
+            self.delta = self.irgn_par["delta"]/np.sqrt(self.par["NSlice"])
             self.omega = self.irgn_par["omega"]
             self._executeIRGN3D()

pyqmri/models/ASL.py
@@ -218,6 +218,7 @@ class Model(BaseModel):
         del_t_min = del_t.min()
         del_t_max = del_t.max()
         ind = 32  # int(images.shape[-1]/2) 30, 60
+        off = 0
         if dim_2D:
             if not self.figure:
                 plt.ion()
@@ -259,7 +260,7 @@ class Model(BaseModel):
                     self.ax[-1].axis('off')
 
                 self.f_plot = self.ax[1].imshow(
-                    (f[int(self.NSlice / 2), ...]))
+                    (f[int(self.NSlice / 2)+off, ...]))
                 self.f_plot_cor = self.ax[7].imshow(
                     (f[:, int(f.shape[1] / 2), ...]))
                 self.f_plot_sag = self.ax[2].imshow(
@@ -278,7 +279,7 @@ class Model(BaseModel):
                 plt.pause(1e-10)
 
                 self.del_t_plot = self.ax[3].imshow(
-                    (del_t[int(self.NSlice / 2), ...]))
+                    (del_t[int(self.NSlice / 2)+off, ...]))
                 self.del_t_plot_cor = self.ax[9].imshow(
                     (del_t[:, int(del_t.shape[1] / 2), ...]))
                 self.del_t_plot_sag = self.ax[4].imshow(
@@ -326,14 +327,15 @@ class Model(BaseModel):
                 plt.show()
                 plt.pause(1e-4)
             else:
-                self.f_plot.set_data((f[int(self.NSlice / 2), ...]))
+                self.f_plot.set_data((f[int(self.NSlice / 2)+off, ...]))
                 self.f_plot_cor.set_data((f[:, int(f.shape[1] / 2), ...]))
                 self.f_plot_sag.set_data(
                     np.flip((f[:, :, int(f.shape[-1] / 2)]).T, 1))
                 self.f_plot.set_clim([f_min, f_max])
                 self.f_plot_cor.set_clim([f_min, f_max])
                 self.f_plot_sag.set_clim([f_min, f_max])
-                self.del_t_plot.set_data((del_t[int(self.NSlice / 2), ...]))
+                self.del_t_plot.set_data((
+                    del_t[int(self.NSlice / 2)+off, ...]))
                 self.del_t_plot_cor.set_data(
                     (del_t[:, int(del_t.shape[1] / 2), ...]))
                 self.del_t_plot_sag.set_data(

pyqmri/models/GeneralModel.py
@@ -155,7 +155,7 @@ class Model(BaseModel):
 
     def plot_unknowns(self, x, dim_2D=False):
         tmp_x = (self.rescale(x))
-        tmp_x[0] = np.abs(tmp_x[0])
+        tmp_x[0] = np.abs(tmp_x[0])#*np.sign(tmp_x[0])
         tmp_x = np.real(tmp_x)
 
         if dim_2D:

pyqmri/operator.py
@@ -1618,8 +1618,7 @@ class OperatorFiniteGradient(Operator):
         self.ctx = self.ctx[0]
         self._ratio = clarray.to_device(
             self.queue,
-            (1 /
-             self.unknowns *
+            (1 *
              np.ones(
                  self.unknowns)).astype(
                      dtype=self.DTYPE_real))
@@ -1689,39 +1688,39 @@ class OperatorFiniteGradient(Operator):
                    self.NSlice *
                    self.dimY *
                    self.dimX * 4))
+        print("TV x: ", np.sum(np.abs(grad)))
         gradnorm = np.sum(np.abs(grad), axis=-1)
-#        gradnorm[gradnorm < 1e-8] = 0
-#        print("Diff between x: ", np.linalg.norm(scale, axis=-1))
-#        print("Diff between grad x: ", gradnorm)
+        print("Diff between grad x: ", gradnorm)
+        gradnorm /= np.sum(gradnorm)/self.unknowns
         scale = 1/gradnorm
         scale[~np.isfinite(scale)] = 1
-        sum_scale = 1 / 1e5
+        print("Scale: ", scale)
         for j in range(x.shape[0])[:self.unknowns_TGV]:
-            self._ratio[j] = scale[j] / sum_scale * self._weights[j]
+            self._ratio[j] = scale[j] * self._weights[j]
 #        sum_scale = np.sqrt(np.sum(np.abs(
 #            scale[self.unknowns_TGV:])**2/(1000)))
         for j in range(x.shape[0])[self.unknowns_TGV:]:
-            self._ratio[j] = scale[j] / sum_scale * self._weights[j]
-#        print("Ratio: ", self._ratio)
-        # x = clarray.to_device(self.queue, x)
-        # grad = clarray.to_device(
-        #     self.queue, np.zeros(x.shape + (4,),
-        #                          dtype=self.DTYPE))
-#        grad.add_event(
-#            self.fwd(
-#                grad,
-#                x,
-#                wait_for=grad.events +
-#                x.events))
-#        x = x.get()
-#        grad = grad.get()
-#        grad = np.reshape(
-#            grad, (self.unknowns,
-#                   self.NSlice *
-#                   self.dimY *
-#                   self.dimX * 4))
-#        print("Norm of grad x: ",  np.sum(np.abs(grad), axis=-1))
-#        print("Total Norm of grad x: ",  np.sum(np.abs(grad)))
+            self._ratio[j] = scale[j] * self._weights[j]
+        print("Ratio: ", self._ratio)
+        x = clarray.to_device(self.queue, x)
+        grad = clarray.to_device(
+            self.queue, np.zeros(x.shape + (4,),
+                                 dtype=self.DTYPE))
+        grad.add_event(
+            self.fwd(
+                grad,
+                x,
+                wait_for=grad.events +
+                x.events))
+        x = x.get()
+        grad = grad.get()
+        grad = np.reshape(
+            grad, (self.unknowns,
+                  self.NSlice *
+                  self.dimY *
+                  self.dimX * 4))
+        print("Norm of grad x: ",  np.sum(np.abs(grad), axis=-1))
+        print("Total Norm of grad x: ",  np.sum(np.abs(grad)))
 
 
 class OperatorFiniteSymGradient(Operator):
@@ -1779,7 +1778,7 @@ class OperatorFiniteGradientStreamed(Operator):
             self._ratio.append(
                 clarray.to_device(
                     self.queue[4*j],
-                    (1 / self.unknowns *
+                    (1 *
                      np.ones(self.unknowns)).astype(dtype=DTYPE_real)))
 
         self.unknown_shape = (self.NSlice, self.unknowns, self.dimY, self.dimX)
@@ -1831,9 +1830,10 @@ class OperatorFiniteGradientStreamed(Operator):
                    self.dimY *
                    self.dimX * 4))
         gradnorm = np.sum(np.abs(grad), axis=-1)
+        gradnorm /= np.sum(gradnorm)
         scale = 1 / gradnorm
         scale[~np.isfinite(scale)] = 1
-        sum_scale = 1 / 1e5
+        sum_scale = 1
 
         for i in range(self.num_dev):
             for j in range(inp.shape[0])[:self.unknowns_TGV]:

pyqmri/pyqmri.py
@@ -238,13 +238,13 @@ def _genImages(myargs, par, data, off):
 
 def _estScaleNorm(myargs, par, images, data):
     if myargs.imagespace:
-        dscale = DTYPE_real(np.sqrt(2*1e3) /
+        dscale = DTYPE_real(np.sqrt(2) /
                             (np.linalg.norm(images.flatten())))
         par["dscale"] = dscale
         images = images*dscale
     else:
-        dscale = (DTYPE_real(np.sqrt(2*1e3)) /
-                  (np.linalg.norm(data.flatten())))
+        dscale = DTYPE_real(np.sqrt(2) /
+                            (np.linalg.norm(data.flatten())))
         par["dscale"] = dscale
         images = images*dscale
         data = data*dscale
@@ -292,14 +292,14 @@ def _estScaleNorm(myargs, par, images, data):
             sig = np.sum(
                 tmp[..., int(par["NSlice"]/2/par["MB"]), ind] *
                 np.conj(
-                    data[...,
-                         int(par["NSlice"]/2/par["MB"]), ind]))/np.sum(ind)
+                    tmp[...,
+                        int(par["NSlice"]/2/par["MB"]), ind]))/np.sum(ind)
             noise = np.sum(
                 tmp[..., int(par["NSlice"]/2/par["MB"]), ~ind] *
                 np.conj(
-                    data[...,
-                         int(par["NSlice"]/2/par["MB"]), ~ind]))/np.sum(~ind)
-        SNR_est = np.abs(sig/noise)
+                    tmp[...,
+                        int(par["NSlice"]/2/par["MB"]), ~ind]))/np.sum(~ind)
+        SNR_est = np.abs(sig/noise)#*par["mask"].size/np.sum(par["mask"])
         par["SNR_est"] = SNR_est
         print("Estimated SNR from kspace", SNR_est)
 
@@ -342,15 +342,17 @@ def _readInput(myargs, par):
     if not os.path.exists(outdir):
         os.makedirs(outdir)
     par["outdir"] = outdir
-    par["file"] = h5py.File(file)
+    par["file"] = h5py.File(file, 'a')
 
 
 def _start_recon(myargs):
     """
-    The Main Function. Reads in the data and
-    starts the model based reconstruction.
+    Model based reconstruction main function.
 
-    Args:
+    Reads in the data and starts the model based reconstruction.
+
+    Args
+    ----
       myargs:
         Arguments from pythons argparse to modify the behaviour of the
         reconstruction procedure.
@@ -505,10 +507,12 @@ def _start_recon(myargs):
         par["Nproj_measured"] = Nproj
         Nproj = Nproj_new
         data = np.require(np.transpose(np.reshape(data[..., :Nproj*NScan, :],
-                                       (NC, reco_Slices, NScan, Nproj, N)),
+                                                  (NC, reco_Slices, NScan,
+                                                   Nproj, N)),
                                        (2, 0, 1, 3, 4)), requirements='C')
         par["traj"] = np.require(np.reshape(par["traj"][:Nproj*NScan, :],
-                                 (NScan, Nproj, N)), requirements='C')
+                                            (NScan, Nproj, N)),
+                                 requirements='C')
         par["dcf"] = np.sqrt(np.array(goldcomp.cmp(par["traj"]),
                                       dtype=DTYPE_real)).astype(DTYPE_real)
         par["dcf"] = np.require(np.abs(par["dcf"]), DTYPE_real,
@@ -598,7 +602,7 @@ def _start_recon(myargs):
 ###############################################################################
 # Init forward model and initial guess ########################################
 ###############################################################################
-#    del par["file"]["images"]
+    # del par["file"]["images"]
 
     if myargs.sig_model == "GeneralModel":
         par["modelfile"] = myargs.modelfile
@@ -620,7 +624,7 @@ def _start_recon(myargs):
         par["weights"] = np.ones((par["unknowns"]), dtype=np.float32)
     else:
         par["weights"] = np.array(myargs.weights, dtype=np.float32)
-    par["weights"] = par["weights"]/par["unknowns"]
+    par["weights"] = par["weights"]
 ###############################################################################
 # Compute initial guess #######################################################
 ###############################################################################
@@ -641,7 +645,6 @@ def _start_recon(myargs):
         opt.data = images
     else:
         opt.data = data
-
     f = h5py.File(par["outdir"]+"output_" + par["fname"], "a")
     f.create_dataset("images_ifft", data=images)
     f.attrs['data_norm'] = par["dscale"]
@@ -675,10 +678,12 @@ def run(recon_type='3D', reg_type='TGV', slices=1, trafo=True,
         modelfile="models.ini", modelname="VFA-E1",
         fft_dim=-1):
     """
-    Start a 3D model based reconstruction. Data can also be selected at
-    start up.
+    Start a 3D model based reconstruction.
 
-    Args:
+    Start a 3D model based reconstruction. Data can be selected at start up.
+
+    Args
+    ----
       recon_type (str):
         3D (2D currently not supported but 3D works on one slice also)
       reg_type (str):