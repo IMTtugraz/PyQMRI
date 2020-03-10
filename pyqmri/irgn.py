Fixes to scaling

pyqmri/irgn.py
@@ -264,7 +264,7 @@ class IRGNOptimizer:
         self.irgn_par["delta"] = np.minimum(
             self.delta
             * self.irgn_par["delta_inc"]**ign,
-            self.irgn_par["delta_max"]/np.sqrt(self.par["NSlice"]))
+            self.irgn_par["delta_max"]/(self.par["NSlice"]))
         self.irgn_par["gamma"] = np.maximum(
             self.gamma * self.irgn_par["gamma_dec"]**ign,
             self.irgn_par["gamma_min"])
@@ -466,9 +466,9 @@ class IRGNOptimizer:
                   3D can be used with a single slice.")
             raise NotImplementedError
         else:
-            self.irgn_par["lambd"] *= (np.sqrt(self.par["NSlice"])
+            self.irgn_par["lambd"] *= ((self.par["NSlice"])
                                        / np.sqrt(self.par["SNR_est"]))
             self.gamma = self.irgn_par["gamma"]
-            self.delta = self.irgn_par["delta"]/np.sqrt(self.par["NSlice"])
+            self.delta = self.irgn_par["delta"]/(self.par["NSlice"])
             self.omega = self.irgn_par["omega"]
             self._executeIRGN3D()

pyqmri/pyqmri.py
@@ -88,10 +88,11 @@ def _precoompFFT(data, par):
               "Precompute FFT along X and Y")
         data = np.fft.ifft2(data, norm='ortho')
         par["fft_dim"] = None
-
     else:
         par["fft_dim"] = [-2, -1]
-    return data
+
+    return np.require(data.astype(DTYPE),
+                      requirements='C')
 
 
 def _setupOCL(myargs, par):