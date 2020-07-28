799f6ab31b19e5e20fbbfcf876c6433267cda93c
Maier, Oliver <oliver.maier@tugraz.at>
2020-07-28 11:24:27 +0200

    Version increment

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='pyqmri',
-      version='0.3.0',
+      version='0.3.1',
       description='Model-based parameter quantification using OpenCL and Python',
       long_description=long_description,
       long_description_content_type="text/x-rst",
