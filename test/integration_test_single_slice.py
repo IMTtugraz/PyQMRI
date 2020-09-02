2b4caec763aacef94e794cd33836323c1d560c82
Maier, Oliver <oliver.maier@tugraz.at>
2020-09-02 15:46:37 +0200

    Splitup integration tests due to memory overflow

--- test/integration_test_single_slice.py
+++ test/integration_test_single_slice.py
@@ -57,43 +57,8 @@ def test_VFA_model_kspace_TGV_cart():
 
 
 @pytest.mark.integration_test
-def test_VFA_model_kspace_TGV_cart_multislice(gen_multislice_data):
-    assert pyqmri.run(data=os.getcwd()+'/test/VFA_cart_test.h5',
-                      model='VFA',
-                      config=os.getcwd()+'/test/default.ini',
-                      trafo=False,
-                      slices=4
-                      ) is None
-
-
-@pytest.mark.integration_test
-def test_VFA_model_kspace_TGV_cart_multislice_streamed(gen_multislice_data):
-    assert pyqmri.run(data=os.getcwd()+'/test/VFA_cart_test.h5',
-                      model='VFA',
-                      config=os.getcwd()+'/test/default.ini',
-                      trafo=False,
-                      slices=4,
-                      streamed=1,
-                      devices=0,
-                      ) is None
-
-
-@pytest.mark.integration_test
-def test_VFA_model_kspace_TV_cart_multislice_streamed(gen_multislice_data):
-    assert pyqmri.run(data=os.getcwd()+'/test/VFA_cart_test.h5',
-                      model='VFA',
-                      config=os.getcwd()+'/test/default.ini',
-                      trafo=False,
-                      slices=4,
-                      streamed=1,
-                      devices=0,
-                      reg_type='TV'
-                      ) is None
-
-
-@pytest.mark.integration_test
 def test_VFA_model_kspace_TGV_cart_imageguess_CG(
-        gen_multislice_data_noimageguess):
+        gen_noimageguess):
     assert pyqmri.run(data=os.getcwd()+'/test/VFA_cart_test_imageguess.h5',
                       model='VFA',
                       config=os.getcwd()+'/test/default.ini',
@@ -103,7 +68,7 @@ def test_VFA_model_kspace_TGV_cart_imageguess_CG(
 
 @pytest.mark.integration_test
 def test_VFA_model_kspace_TGV_cart_imageguess(
-        gen_multislice_data_noimageguess):
+        gen_noimageguess):
     assert pyqmri.run(data=os.getcwd()+'/test/VFA_cart_test_imageguess.h5',
                       model='VFA',
                       config=os.getcwd()+'/test/default.ini',
@@ -114,7 +79,7 @@ def test_VFA_model_kspace_TGV_cart_imageguess(
 
 @pytest.mark.integration_test
 def test_VFA_model_kspace_TGV_cart_coilguess(
-        gen_multislice_data_nocoils):
+        gen_data_nocoils):
     assert pyqmri.run(data=os.getcwd()+'/test/VFA_cart_test_coilguess.h5',
                       model='VFA',
                       config=os.getcwd()+'/test/default.ini',
@@ -125,7 +90,7 @@ def test_VFA_model_kspace_TGV_cart_coilguess(
 
 @pytest.mark.integration_test
 def test_VFA_model_kspace_TGV_cart_coilguess_radial(
-        gen_multislice_data_nocoils_radial):
+        gen_data_nocoils_radial):
     assert pyqmri.run(
         data=os.getcwd()+'/test/VFA_radial_test_coilguess.h5',
         model='VFA',
@@ -136,46 +101,7 @@ def test_VFA_model_kspace_TGV_cart_coilguess_radial(
 
 
 @pytest.fixture(scope="function")
-def gen_multislice_data():
-    file = h5py.File(os.getcwd()+'/test/VFA_cart_smalltest.h5', 'r')
-
-    Coils = file["Coils"][()]
-    real_dat = file["real_dat"][()]
-    imag_dat = file["imag_dat"][()]
-    images = file["images"][()]
-    fa_corr = file["fa_corr"][()]
-
-    image_dimensions = file.attrs["image_dimensions"]
-    fa = file.attrs["fa"][()]
-    TR = file.attrs["TR"]
-
-    file_out = h5py.File(os.getcwd()+'/test/VFA_cart_test.h5', 'w')
-
-    slices = 4
-
-    Coils = np.repeat(Coils, repeats=slices, axis=1)
-    real_dat = np.repeat(real_dat, repeats=slices, axis=2)
-    imag_dat = np.repeat(imag_dat, repeats=slices, axis=2)
-    fa_corr = np.repeat(fa_corr, repeats=slices, axis=0)
-    images = np.repeat(images, repeats=slices, axis=1)
-
-    file_out["Coils"] = Coils
-    file_out["real_dat"] = real_dat
-    file_out["imag_dat"] = imag_dat
-    file_out["fa_corr"] = fa_corr
-    file_out["images"] = images
-
-    image_dimensions[2] = slices
-
-    file_out.attrs["TR"] = TR
-    file_out.attrs["fa"] = fa
-    file_out.attrs["flip_angle(s)"] = fa
-    file_out.attrs["image_dimensions"] = image_dimensions
-    file_out.close()
-    file.close()
-
-@pytest.fixture(scope="function")
-def gen_multislice_data_noimageguess():
+def gen_noimageguess():
     file = h5py.File(os.getcwd()+'/test/VFA_cart_smalltest.h5', 'r')
 
     Coils = file["Coils"][()]
@@ -189,7 +115,7 @@ def gen_multislice_data_noimageguess():
 
     file_out = h5py.File(os.getcwd()+'/test/VFA_cart_test_imageguess.h5', 'w')
 
-    slices = 4
+    slices = 1
 
     Coils = np.repeat(Coils, repeats=slices, axis=1)
     real_dat = np.repeat(real_dat, repeats=slices, axis=2)
@@ -210,8 +136,9 @@ def gen_multislice_data_noimageguess():
     file_out.close()
     file.close()
 
+
 @pytest.fixture(scope="function")
-def gen_multislice_data_nocoils():
+def gen_data_nocoils():
     file = h5py.File(os.getcwd()+'/test/VFA_cart_smalltest.h5', 'r')
 
     real_dat = file["real_dat"][()]
@@ -224,7 +151,7 @@ def gen_multislice_data_nocoils():
 
     file_out = h5py.File(os.getcwd()+'/test/VFA_cart_test_coilguess.h5', 'w')
 
-    slices = 4
+    slices = 1
 
     real_dat = np.repeat(real_dat, repeats=slices, axis=2)
     imag_dat = np.repeat(imag_dat, repeats=slices, axis=2)
@@ -243,8 +170,9 @@ def gen_multislice_data_nocoils():
     file_out.close()
     file.close()
 
+
 @pytest.fixture(scope="function")
-def gen_multislice_data_nocoils_radial():
+def gen_data_nocoils_radial():
     file = h5py.File(os.getcwd()+'/test/smalltest.h5', 'r')
 
     real_dat = file["real_dat"][()]
@@ -260,7 +188,7 @@ def gen_multislice_data_nocoils_radial():
 
     file_out = h5py.File(os.getcwd()+'/test/VFA_radial_test_coilguess.h5', 'w')
 
-    slices = 4
+    slices = 1
 
     real_dat = np.repeat(real_dat, repeats=slices, axis=2)
     imag_dat = np.repeat(imag_dat, repeats=slices, axis=2)
