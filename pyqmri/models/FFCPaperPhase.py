e66dfd9aa7f74ab0714232aab810b066f277befd
Maier, Oliver <oliver.maier@tugraz.at>
2020-11-26 12:06:02 +0100

    Updated initial guess. Working with all data sets currently

--- pyqmri/models/FFCPaperPhase.py
+++ pyqmri/models/FFCPaperPhase.py
@@ -344,7 +344,7 @@ class Model(BaseModel):
         self.dscale = args[1]
         self.images = np.reshape(np.abs(args[0]/args[1]),
                                  self.t.shape+args[0].shape[-3:])
-        test_M0 = 1*np.ones(
+        test_M0 = 0.1*np.ones(
             (self.NSlice, self.dimY, self.dimX), dtype=self._DTYPE)
         self.constraints[0].update(1/args[1])
         test_Xi = 1*np.ones(
