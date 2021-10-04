2efb703f9d117ca959084cd853bc0df15a10021f
Oliver Maier <oliver.maier@tugraz.at>
2021-10-04 13:47:56 +0200

    Version increment to include missing kernels

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='pyqmri',
-      version='1.2.1',
+      version='1.2.2',
       description='Model-based parameter quantification using OpenCL and Python',
       long_description=long_description,
       long_description_content_type="text/x-rst",
