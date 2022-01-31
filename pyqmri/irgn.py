Fixed bug in streamed reco. Missing scaling factor in kernels.

pyqmri/irgn.py
@@ -341,6 +341,11 @@ class IRGNOptimizer:
                     if self._reg_type == 'TGV':
                         self._pdop._symgrad_op.precond = False
                 self._balanceModelGradientsNorm(result, ign)
+                # scale = (result*np.array(self._model.uk_scale)[:,None,None,None]).reshape(self.par["unknowns"], -1)
+                # scale = np.linalg.norm(np.abs(scale), axis=-1)
+                # scale = 1e2/scale
+                # scale[~np.isfinite(scale)] = 1
+                # self._pdop._op.updateRatio(np.array(self._model.uk_scale)*scale)
             
             self._updateIRGNRegPar(ign)
             if self.precond:
@@ -402,19 +407,19 @@ class IRGNOptimizer:
             self.irgn_par["delta"] = np.minimum(
                 self._delta
                 * self.irgn_par["delta_inc"]**ign,
-                self.irgn_par["delta_max"])*self.irgn_par["lambd"]
+                self.irgn_par["delta_max"])*(self.irgn_par["lambd"])
         except OverflowError:
             self.irgn_par["delta"] = np.minimum(
                 np.finfo(self._delta).max,
-                self.irgn_par["delta_max"])
+                self.irgn_par["delta_max"])*(self.irgn_par["lambd"])
 
         
         self.irgn_par["gamma"] = np.maximum(
             self._gamma * self.irgn_par["gamma_dec"]**ign,
-            self.irgn_par["gamma_min"])/self.irgn_par["lambd"]
+            self.irgn_par["gamma_min"])/(self.irgn_par["lambd"])
         self.irgn_par["omega"] = np.maximum(
             self._omega * self.irgn_par["omega_dec"]**ign,
-            self.irgn_par["omega_min"])/self.irgn_par["lambd"]
+            self.irgn_par["omega_min"])/(self.irgn_par["lambd"])
 
     def _balanceModelGradients(self, result, ign):
         

pyqmri/models/FFCsat.py
@@ -13,21 +13,20 @@ class Model(BaseModel):
         super().__init__(par)
         self.constraints = []
         self.t = par["t"]
-        self.b = par["b"]
-        if "b0" in par.keys():
-            self.b0 = par["b0"]
-        else:
-            self.b0 = self.b[0]
+        self.b_evo = par["b_evo"]
+        self.b_pol = par["b_pol"]
+
 
         if len(self.t.shape) < 2:
-            self.b = self.b[None]
+            self.b_pol = self.b_pol[None]
+            self.b_evo = self.b_evo[None]
             self.t = self.t[None]
 
-        self.numT1Scale = len(self.b)
-        self.numC = 1#len(self.b)
-        self.numAlpha = len(self.b)
+        self.numT1Scale = len(self.b_pol)
+        self.numC = 1#len(self.b_pol)
+        self.numAlpha = 1#len(self.b_pol)
 
-        par["unknowns_TGV"] = self.numC + self.numAlpha + len(self.b)
+        par["unknowns_TGV"] = self.numC + self.numAlpha + len(self.b_pol)
         par["unknowns_H1"] = 0
         par["unknowns"] = par["unknowns_TGV"]+par["unknowns_H1"]
 
@@ -46,21 +45,21 @@ class Model(BaseModel):
                             False))
         for j in range(self.numAlpha):
             self.constraints.append(
-                constraints(0.1,
-                            1.2,
-                            False))
+                constraints(0,
+                            2,
+                            True))
         for j in range(self.numT1Scale):
             self.constraints.append(
                 constraints(t1min,
-                            1000*np.abs(np.log(self.b[0])/np.log(self.b[j])).squeeze(),
+                            500,
                             True))
         self._ind1 = 0
         self._ind2 = 0
         self._labels = []
-        for j in range(len(self.b)):
+        for j in range(len(self.b_pol)):
             self._labels.append(
-                "Field "+str(np.round(self.b[j]*1e3, 2))+" mT")
-        par["weights"] = 1e3*np.array([1]*self.numC+self.numAlpha*[1]+self.numT1Scale*[1],dtype=par["DTYPE_real"])
+                "Evo Field "+str(np.round(self.b_evo[j]*1e3, 2))+" mT")
+        par["weights"] = 1*np.array([1]*self.numC+self.numAlpha*[10]+self.numT1Scale*[1],dtype=par["DTYPE_real"])
         # par["weights"] *= 1/np.sum(par["weights"])
 
     def rescale(self, x):
@@ -90,15 +89,15 @@ class Model(BaseModel):
         t = self.t[0][:, None, None, None]
 
 
-        for j in range(len(self.b)):
+        for j in range(len(self.b_pol)):
             offset = len(self.t[j])
             t = self.t[j][:, None, None, None]
             S[offset*(j):offset*(j+1)] = (
                 x[np.mod(j,self.numC)] * self.uk_scale[np.mod(j,self.numC)]
-                * (-self.b0 * x[self.numC+np.mod(j,self.numAlpha)]*self.uk_scale[self.numC+np.mod(j,self.numAlpha)] *
+                * (self.b_pol[j] * x[self.numC+np.mod(j,self.numAlpha)]*self.uk_scale[self.numC+np.mod(j,self.numAlpha)] *
                    np.exp(-t / (x[-self.numT1Scale+j] * self.uk_scale[-self.numT1Scale+j]))
                    + (1 - np.exp(-t / (x[-self.numT1Scale+j] * self.uk_scale[-self.numT1Scale+j])))
-                   * self.b[j])
+                   * self.b_evo[j])
                 )
         S[~np.isfinite(S)] = 0
         S = np.array(S, dtype=self._DTYPE)
@@ -120,15 +119,15 @@ class Model(BaseModel):
         t = self.t[0][:, None, None, None]
 
 
-        for j in range(len(self.b)):
+        for j in range(len(self.b_pol)):
             offset = len(self.t[j])
             t = self.t[j][:, None, None, None]
             grad[np.mod(j,self.numC), offset*(j):offset*(j+1)] = (
                 self.uk_scale[np.mod(j,self.numC)]
-                * (-self.b0 * x[self.numC+np.mod(j,self.numAlpha)]*self.uk_scale[self.numC+np.mod(j,self.numAlpha)] *
+                * (self.b_pol[j] * x[self.numC+np.mod(j,self.numAlpha)]*self.uk_scale[self.numC+np.mod(j,self.numAlpha)] *
                    np.exp(-t / (x[-self.numT1Scale+j] * self.uk_scale[-self.numT1Scale+j]))
                    + (1 - np.exp(-t / (x[-self.numT1Scale+j] * self.uk_scale[-self.numT1Scale+j])))
-                   * self.b[j])
+                   * self.b_evo[j])
                 )
         grad[~np.isfinite(grad)] = 0
 
@@ -141,12 +140,12 @@ class Model(BaseModel):
         t = self.t[0][:, None, None, None]
 
 
-        for j in range(len(self.b)):
+        for j in range(len(self.b_pol)):
             offset = len(self.t[j])
             t = self.t[j][:, None, None, None]
             grad[np.mod(j,self.numAlpha), offset*(j):offset*(j+1)] = (
                 x[np.mod(j,self.numC)] * self.uk_scale[np.mod(j,self.numC)]*self.uk_scale[self.numC+np.mod(j,self.numAlpha)]
-                * (-self.b0 *
+                * (self.b_pol[j] *
                    np.exp(-t / (x[-self.numT1Scale+j] * self.uk_scale[-self.numT1Scale+j]))
                    )
                 )
@@ -159,14 +158,14 @@ class Model(BaseModel):
             (self.numT1Scale, self.NScan, self.NSlice, self.dimY, self.dimX),
             dtype=self._DTYPE)
         t = self.t[0][:, None, None, None]
-        for j in range(len(self.b)):
+        for j in range(len(self.b_pol)):
             offset = len(self.t[j])
             t = self.t[j][:, None, None, None]
             grad[j, (j)*offset:(j+1)*offset] = (
                 x[np.mod(j,self.numC)]*self.uk_scale[np.mod(j,self.numC)]*(
-                    -self.b0*t*x[self.numC+np.mod(j,self.numAlpha)]*self.uk_scale[self.numC+np.mod(j,self.numAlpha)]
+                    self.b_pol[j]*t*x[self.numC+np.mod(j,self.numAlpha)]*self.uk_scale[self.numC+np.mod(j,self.numAlpha)]
                     * np.exp(-t/(x[-self.numT1Scale+j]*self.uk_scale[-self.numT1Scale+j]))
-                    - self.b[j]*t
+                    - self.b_evo[j]*t
                     * np.exp(-t/(x[-self.numT1Scale+j]*self.uk_scale[-self.numT1Scale+j]))
                     )
                 )/(x[-self.numT1Scale+j]**2*self.uk_scale[-self.numT1Scale+j])
@@ -227,7 +226,7 @@ class Model(BaseModel):
                 self.plot_ax = plt.subplot(self.gs[-1, :])
                 self.plot_ax.set_title("Time course", color='w')
                 self.time_course_ref = []
-                for j in range(len(self.b)):
+                for j in range(len(self.b_pol)):
                     self.time_course_ref.append(self.plot_ax.plot(
                         self.t[j], np.real(
                             self.images[j, :,
@@ -277,7 +276,7 @@ class Model(BaseModel):
                     self._plot[j].set_clim(
                         [tmp_x[j].min(), tmp_x[j].max()])
 
-                for j in range(len(self.b)):
+                for j in range(len(self.b_pol)):
                     self.time_course[j].set_ydata(
                         np.real(images[
                             j, :, int(self.NSlice/2), self._ind2, self._ind1]))
@@ -305,7 +304,7 @@ class Model(BaseModel):
         if event.inaxes in self.ax[::3]:
             self._ind1 = int(event.xdata)
             self._ind2 = int(event.ydata)
-            for j in range(len(self.b)):
+            for j in range(len(self.b_pol)):
                 self.time_course_ref[j].set_ydata(np.real(
                         self.images[j, :,
                                     int(self.NSlice/2),
@@ -321,20 +320,20 @@ class Model(BaseModel):
         
         test_M0 = []
         for j in range(self.numC):
-            test_M0.append(1*np.ones(kwargs['images'].shape[-3:], dtype=self._DTYPE)*
+            test_M0.append(10*np.ones(kwargs['images'].shape[-3:], dtype=self._DTYPE)*
                 np.exp(1j*np.angle(kwargs['images'][0])))
             # self.constraints[j].update(1/kwargs['dscale'])
         test_Xi = []
         for j in range(self.numAlpha):
             test_Xi.append(
-                0.1 *
+                1 *
                 np.ones(kwargs['images'].shape[-3:], dtype=self._DTYPE))
  
         test_R1 = []
-        # self.b *= args[1]
+        # self.b_pol *= args[1]
         for j in range(self.numT1Scale):
             test_R1.append(
-                50 *
+                10 *
                 np.ones(kwargs['images'].shape[-3:], dtype=self._DTYPE))
             
             

pyqmri/models/IvimDTI.py
@@ -73,7 +73,7 @@ class Model(BaseModel):
         self.constraints.append(
             constraints(
                 0 / self.uk_scale[0],
-                10 / self.uk_scale[0],
+                1e10 / self.uk_scale[0],
                 False))
         self.constraints.append(
             constraints(
@@ -193,7 +193,7 @@ class Model(BaseModel):
 
         S *= self.phase
         S[~np.isfinite(S)] = 0
-        return S
+        return S*self.dscale
 
     def _execute_gradient_3D(self, x):
         ADC = x[1, ...]**2 * self.uk_scale[1]**2 * self.dir[..., 0]**2 + \
@@ -275,14 +275,15 @@ class Model(BaseModel):
              grad_ADC_ivim], dtype=self._DTYPE)
         grad[~np.isfinite(grad)] = 0
         grad *= self.phase
-        return grad
+        return grad*self.dscale
 
     def computeInitialGuess(self, **kwargs):
         self.phase = np.exp(1j*(np.angle(kwargs['images'])-np.angle(kwargs['images'][0])))
+        self.dscale = kwargs["dscale"]
         if self.b0 is not None:
             test_M0 = self.b0
         else:
-            test_M0 = kwargs['images'][0]
+            test_M0 = kwargs['images'][0]/self.dscale
         
         if np.allclose(kwargs['initial_guess'],-1):
             #default setting

pyqmri/operator.py
@@ -111,6 +111,7 @@ class Operator(ABC):
                                self.dimX)
         self._overlap = 0
         self.ratio = []
+        self._weights = par["weights"]
         for j in range(self.num_dev):
             self.ratio.append(
                 clarray.to_device(
@@ -309,6 +310,13 @@ class Operator(ABC):
                         DTYPE_real=DTYPE_real)
                 FT = op.NUFFT
         return op, FT
+    
+    def updateRatio(self, inp):
+        for j in range(self.num_dev):
+            self.ratio = clarray.to_device(
+                self.queue[4*j],
+                (self._weights*np.array(inp)).astype(
+                         dtype=self.DTYPE_real))
 
     @staticmethod
     def GradientOperatorFactory(par,
@@ -506,7 +514,7 @@ class OperatorImagespace(Operator):
 
     def __init__(self, par, prg, DTYPE=np.complex64, DTYPE_real=np.float32):
         super().__init__(par, prg, DTYPE, DTYPE_real)
-        self.queue = self.queue[0]
+        self.queue = self.queue
         self.ctx = self.ctx[0]
         self._out_shape_fwd = (self.NScan, self.NSlice, self.dimY, self.dimX)
 
@@ -537,7 +545,7 @@ class OperatorImagespace(Operator):
         else:
             wait_for = []
         return self.prg.operator_fwd_imagespace(
-            self.queue, (self.NSlice, self.dimY, self.dimX), None,
+            self.queue[0], (self.NSlice, self.dimY, self.dimX), None,
             out.data, inp[0].data, inp[2].data,
             np.int32(self.NScan),
             np.int32(self.unknowns),
@@ -569,10 +577,10 @@ class OperatorImagespace(Operator):
         else:
             wait_for = []
         tmp_result = clarray.zeros(
-            self.queue, (self.NScan, self.NSlice, self.dimY, self.dimX),
+            self.queue[0], (self.NScan, self.NSlice, self.dimY, self.dimX),
             self.DTYPE, "C")
         self.prg.operator_fwd_imagespace(
-            self.queue, (self.NSlice, self.dimY, self.dimX), None,
+            self.queue[0], (self.NSlice, self.dimY, self.dimX), None,
             tmp_result.data, inp[0].data, inp[2].data,
             np.int32(self.NScan),
             np.int32(self.unknowns),
@@ -637,7 +645,7 @@ class OperatorImagespace(Operator):
         else:
             wait_for = []
         out = clarray.zeros(
-            self.queue, (self.unknowns, self.NSlice, self.dimY, self.dimX),
+            self.queue[0], (self.unknowns, self.NSlice, self.dimY, self.dimX),
             dtype=self.DTYPE)
         self.prg.operator_ad_imagespace(
             out.queue, (self.NSlice, self.dimY, self.dimX), None,
@@ -673,7 +681,7 @@ class OperatorImagespace(Operator):
             wait_for = []
         
         return self.prg.update_Kyk1_imagespace(
-            self.queue, (self.NSlice, self.dimY, self.dimX), None,
+            self.queue[0], (self.NSlice, self.dimY, self.dimX), None,
             out.data, inp[0].data, inp[3].data, inp[1].data,
             np.int32(self.NScan),
             np.int32(self.unknowns),
@@ -722,10 +730,10 @@ class OperatorKspace(Operator):
     def __init__(self, par, prg, DTYPE=np.complex64,
                  DTYPE_real=np.float32, trafo=True):
         super().__init__(par, prg, DTYPE, DTYPE_real)
-        self.queue = self.queue[0]
+        # self.queue = self.queue[0]
         self.ctx = self.ctx[0]
         self._tmp_result = clarray.zeros(
-            self.queue, (self.NScan, self.NC,
+            self.queue[0], (self.NScan, self.NC,
                          self.NSlice, self.dimY, self.dimX),
             self.DTYPE, "C")
         if not trafo:
@@ -738,7 +746,7 @@ class OperatorKspace(Operator):
             self._out_shape_fwd = (self.NScan, self.NC,
                          self.NSlice, self.Nproj, self.N)
         self.NUFFT = CLnuFFT.create(self.ctx,
-                                    self.queue,
+                                    self.queue[0],
                                     par,
                                     radial=trafo,
                                     DTYPE=DTYPE,
@@ -772,7 +780,7 @@ class OperatorKspace(Operator):
             wait_for = []
         self._tmp_result.add_event(
             self.prg.operator_fwd(
-                self.queue,
+                self.queue[0],
                 (self.NSlice, self.dimY, self.dimX),
                 None,
                 self._tmp_result.data, inp[0].data,
@@ -815,7 +823,7 @@ class OperatorKspace(Operator):
             wait_for = []
         self._tmp_result.add_event(
             self.prg.operator_fwd(
-                self.queue,
+                self.queue[0],
                 (self.NSlice, self.dimY, self.dimX),
                 None,
                 self._tmp_result.data, inp[0].data,
@@ -826,7 +834,7 @@ class OperatorKspace(Operator):
                 wait_for=(self._tmp_result.events + inp[0].events
                           + wait_for)))
         tmp_sino = clarray.zeros(
-            self.queue,
+            self.queue[0],
             self._out_shape_fwd,
             self.DTYPE, "C")
         self.NUFFT.FFT(tmp_sino, self._tmp_result,
@@ -864,7 +872,7 @@ class OperatorKspace(Operator):
                                                     + inp[0].events)))
 
         return self.prg.operator_ad(
-            self.queue, (self.NSlice, self.dimY, self.dimX), None,
+            self.queue[0], (self.NSlice, self.dimY, self.dimX), None,
             out.data, self._tmp_result.data, inp[1].data,
             inp[2].data, np.int32(self.NC),
             np.int32(self.NScan),
@@ -901,7 +909,7 @@ class OperatorKspace(Operator):
                 self._tmp_result, inp[0], wait_for=(wait_for
                                                     + inp[0].events)))
         out = clarray.zeros(
-            self.queue, (self.unknowns, self.NSlice, self.dimY, self.dimX),
+            self.queue[0], (self.unknowns, self.NSlice, self.dimY, self.dimX),
             dtype=self.DTYPE)
         self.prg.operator_ad(
             out.queue, (self.NSlice, self.dimY, self.dimX), None,
@@ -942,7 +950,7 @@ class OperatorKspace(Operator):
                                                     + inp[0].events)))
 
         return self.prg.update_Kyk1(
-            self.queue, (self.NSlice, self.dimY, self.dimX), None,
+            self.queue[0], (self.NSlice, self.dimY, self.dimX), None,
             out.data, self._tmp_result.data, inp[2].data,
             inp[3].data, inp[1].data, np.int32(self.NC),
             np.int32(self.NScan),
@@ -992,11 +1000,11 @@ class OperatorKspaceSMS(Operator):
     def __init__(self, par, prg, DTYPE=np.complex64,
                  DTYPE_real=np.float32):
         super().__init__(par, prg, DTYPE, DTYPE_real)
-        self.queue = self.queue[0]
+        # self.queue = self.queue[0]
         self.ctx = self.ctx[0]
         self.packs = par["packs"]*par["numofpacks"]
         self._tmp_result = clarray.zeros(
-            self.queue, (self.NScan, self.NC,
+            self.queue[0], (self.NScan, self.NC,
                          self.NSlice, self.dimY, self.dimX),
             self.DTYPE, "C")
         self._out_shape_fwd = (self.NScan, self.NC,
@@ -1005,7 +1013,7 @@ class OperatorKspaceSMS(Operator):
         self.Nproj = self.dimY
         self.N = self.dimX
         self.NUFFT = CLnuFFT.create(self.ctx,
-                                    self.queue,
+                                    self.queue[0],
                                     par,
                                     radial=False,
                                     SMS=True,
@@ -1040,7 +1048,7 @@ class OperatorKspaceSMS(Operator):
             wait_for = []
         self._tmp_result.add_event(
             self.prg.operator_fwd(
-                self.queue,
+                self.queue[0],
                 (self.NSlice, self.dimY, self.dimX),
                 None,
                 self._tmp_result.data, inp[0].data,
@@ -1083,7 +1091,7 @@ class OperatorKspaceSMS(Operator):
             wait_for = []
         self._tmp_result.add_event(
             self.prg.operator_fwd(
-                self.queue,
+                self.queue[0],
                 (self.NSlice, self.dimY, self.dimX),
                 None,
                 self._tmp_result.data, inp[0].data,
@@ -1095,7 +1103,7 @@ class OperatorKspaceSMS(Operator):
                           inp[1].events + inp[2].events
                           + wait_for)))
         tmp_sino = clarray.zeros(
-            self.queue,
+            self.queue[0],
             (self.NScan, self.NC, self.packs, self.Nproj, self.N),
             self.DTYPE, "C")
 
@@ -1132,7 +1140,7 @@ class OperatorKspaceSMS(Operator):
                 self._tmp_result, inp[0], wait_for=(wait_for
                                                     + inp[0].events)))
         return self.prg.operator_ad(
-            self.queue, (self.NSlice, self.dimY, self.dimX), None,
+            self.queue[0], (self.NSlice, self.dimY, self.dimX), None,
             out.data, self._tmp_result.data, inp[1].data,
             inp[2].data, np.int32(self.NC),
             np.int32(self.NScan),
@@ -1169,7 +1177,7 @@ class OperatorKspaceSMS(Operator):
                 self._tmp_result, inp[0], wait_for=(wait_for
                                                     + inp[0].events)))
         out = clarray.zeros(
-            self.queue, (self.unknowns, self.NSlice, self.dimY, self.dimX),
+            self.queue[0], (self.unknowns, self.NSlice, self.dimY, self.dimX),
             dtype=self.DTYPE)
         self.prg.operator_ad(
             out.queue, (self.NSlice, self.dimY, self.dimX), None,
@@ -1208,7 +1216,7 @@ class OperatorKspaceSMS(Operator):
                 self._tmp_result, inp[0], wait_for=(wait_for
                                                     + inp[0].events)))
         return self.prg.update_Kyk1(
-            self.queue, (self.NSlice, self.dimY, self.dimX), None,
+            self.queue[0], (self.NSlice, self.dimY, self.dimX), None,
             out.data, self._tmp_result.data, inp[2].data,
             inp[3].data, inp[1].data, np.int32(self.NC),
             np.int32(self.NScan),
@@ -2746,11 +2754,12 @@ class OperatorFiniteGradientStreamed(Operator):
         return self._stream_grad
 
     def updateRatio(self, inp):
-        for j in range(self.num_dev):
-            self.ratio = clarray.to_device(
-                self.queue[4*j],
-                (self._weights*inp).astype(
-                         dtype=self.DTYPE_real))
+        pass
+        # for j in range(self.num_dev):
+        #     self.ratio = clarray.to_device(
+        #         self.queue[4*j],
+        #         (self._weights*inp).astype(
+        #                  dtype=self.DTYPE_real))
     # def updateRatio(self, inp):
     #     x = np.require(np.swapaxes(inp, 0, 1), requirements='C')
     #     grad = np.zeros(x.shape + (4,), dtype=self.DTYPE)
@@ -2991,11 +3000,12 @@ class OperatorFiniteSymGradientStreamed(Operator):
         return self._stream_symgrad
 
     def updateRatio(self, inp):
-        for j in range(self.num_dev):
-            self.ratio = clarray.to_device(
-                self.queue[4*j],
-                (self._weights*inp).astype(
-                         dtype=self.DTYPE_real))
+        pass
+        # for j in range(self.num_dev):
+        #     self.ratio = clarray.to_device(
+        #         self.queue[4*j],
+        #         (self._weights*inp).astype(
+        #                  dtype=self.DTYPE_real))
 
 
 class OperatorSoftSense(Operator):